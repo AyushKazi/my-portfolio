4e3706a1c4a6f71df1a083b50130535202eacb73
AyushKazi <ayushkazishrestha@gmail.com>
2025-03-03 12:26:04 +0545

    add: folders

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,14 @@
+import AboutMe from "@/components/about-me";
+import Navbar from "@/components/navbar";
+import Projects from "@/components/projects";
 import React from "react";
 
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <>
+      <Navbar />
+      <AboutMe />
+      <Projects />
+    </>
+  );
 }
