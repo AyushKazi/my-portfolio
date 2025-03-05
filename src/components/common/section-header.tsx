7856c96e49c8574a0442419925a862e41f2ef4f2
AyushKazi <ayushkazishrestha@gmail.com>
2025-03-05 16:22:20 +0545

    feat: add SectionHeader component and update Skills section for improved layout and styling

---
+++ src/components/common/section-header.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { BsArrowRight, BsLinkedin } from "react-icons/bs";
+import { FaGithubSquare } from "react-icons/fa";
+import { HiDownload } from "react-icons/hi";
+
+const SectionHeader = ({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) => {
+  return (
+    <h1 className="text-xl md:text-2xl lg:text-3xl xl:text-4xl capitalize pb-4 font-semibold text-center">
+      {children}
+    </h1>
+  );
+};
+
+export default SectionHeader;
--- src/components/skills.tsx
+++ src/components/skills.tsx
@@ -1,19 +1,17 @@
 import { techStack } from "@/lib/techstack";
 import React from "react";
 import Section from "./common/section";
+import SectionHeader from "./common/section-header";
 
 const Skills = () => {
   return (
     <div className="pb-8 scroll-mt-[5rem] text-center  " id="skills">
-      <h1 className="text-xl lg:text-2xl xl:text-4xl pb-4 font-semibold">
-        Tech Stack 💻
-      </h1>
-
+      <SectionHeader>TechStack 💻</SectionHeader>
       <div className="flex flex-wrap justify-center max-w-3xl  items-center mx-auto">
         {techStack.map((tech, index) => (
           <p
             key={index}
-            className="px-4 py-2 m-2 text-sm lg:text-base bg-gray-200 text-black/90 rounded-md"
+            className="px-4 py-2 m-2 text-sm bg-gray-100 shadow text-black/90 rounded-md"
           >
             {tech}
           </p>
