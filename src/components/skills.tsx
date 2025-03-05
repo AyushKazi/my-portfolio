fb3b31500e41508f385a26eed8a062886f0840d7
AyushKazi <ayushkazishrestha@gmail.com>
2025-03-05 12:39:20 +0545

    feat: add skills section with tech stack display

--- src/components/skills.tsx
+++ src/components/skills.tsx
@@ -1,7 +1,26 @@
+import { techStack } from "@/lib/techstack";
 import React from "react";
+import Section from "./common/section";
 
 const Skills = () => {
-  return <div className="bg-blue-300 h-[60vh]">Skills</div>;
+  return (
+    <div className="pb-8 scroll-mt-[5rem] text-center  " id="skills">
+      <h1 className="text-xl lg:text-2xl xl:text-4xl pb-4 font-semibold">
+        Tech Stack 💻
+      </h1>
+
+      <div className="flex flex-wrap justify-center max-w-3xl  items-center mx-auto">
+        {techStack.map((tech, index) => (
+          <p
+            key={index}
+            className="px-4 py-2 m-2 text-sm lg:text-base bg-gray-200 text-black/90 rounded-md"
+          >
+            {tech}
+          </p>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Skills;
---
+++ src/lib/techstack.ts
@@ -0,0 +1,20 @@
+export const techStack = [
+  "React",
+  "TypeScript",
+  "TailwindCSS",
+  "Node.js",
+  "Express",
+  "MongoDB",
+  "Sequelize",
+  "Prisma",
+
+  "Next.js",
+
+  "Git",
+  "GitHub",
+  "MySQL",
+
+  "Vercel",
+  "Firebase",
+  "Postman",
+];
