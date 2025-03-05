c82f6084a51321add031cd5ddeae3f979e4b006b
AyushKazi <ayushkazishrestha@gmail.com>
2025-03-05 16:42:03 +0545

    feat: implement experience section with vertical timeline and add experience data

--- src/components/experience.tsx
+++ src/components/experience.tsx
@@ -1,9 +1,39 @@
+"use client";
 import React from "react";
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+import "react-vertical-timeline-component/style.min.css";
+import SectionHeader from "./common/section-header";
+import { experience } from "@/lib/data";
 
 const Experience = () => {
   return (
-    <div className="bg-green-300 h-[60vh] scroll-mt-[5rem]" id="experience">
-      Experience
+    <div className=" scroll-mt-[5rem] pb-12" id="experience">
+      <SectionHeader>Experience 🚀</SectionHeader>
+      <VerticalTimeline lineColor="">
+        {experience.map((exp, index) => (
+          <React.Fragment key={index}>
+            <VerticalTimelineElement
+              className="vertical-timeline-element--work"
+              contentStyle={{
+                background: "#f3f4f6",
+              }}
+              contentArrowStyle={{
+                borderRight: "7px solid  #9ca3af",
+              }}
+              date={exp.date}
+              icon={exp.icon}
+              iconStyle={{ background: "white", fontSize: "1.5rem" }}
+            >
+              <h3 className="font-semibold capitalize">{exp.title}</h3>
+              <h4 className="">{exp.location}</h4>
+              <p className="!text-base text-gray-600">{exp.description}</p>
+            </VerticalTimelineElement>
+          </React.Fragment>
+        ))}
+      </VerticalTimeline>
     </div>
   );
 };
---
+++ src/lib/data.ts
@@ -0,0 +1,22 @@
+import React from "react";
+import { CgWorkAlt } from "react-icons/cg";
+import { FaReact } from "react-icons/fa";
+
+export const experience = [
+  {
+    title: "Front-End Web Developer Intern",
+    location: "Pokhara, Nepal",
+    description:
+      "I worked as a front-end developer for 2 years in 1 job and 1 year in another job. I also upskilled to the full stack.",
+    icon: React.createElement(CgWorkAlt),
+    date: "Jan 2024 - July 2024",
+  },
+  {
+    title: "Front-End Web Developer",
+    location: "Pokhara, Nepal",
+    description:
+      "I'm now a full-stack developer working as a freelancer. My stack includes React, Next.js, TypeScript, Tailwind, Prisma and MongoDB. I'm open to full-time opportunities.",
+    icon: React.createElement(FaReact),
+    date: "July 2024 - Feb 2025",
+  },
+];
