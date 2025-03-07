f1e183e4a280ab64f6518259ebb240af611cef8b
AyushKazi <ayushkazishrestha@gmail.com>
2025-03-07 12:29:25 +0545

    refactor: adjust Navbar component layout and styling for improved responsiveness

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -11,15 +11,15 @@ const Navbar = () => {
   const pathname = usePathname();
   return (
     <nav className="sticky top-6 z-50  ">
-      <div className=" flex items-center justify-center h-[3.5rem]  sm:px-6 lg:px-8">
+      <div className=" flex items-center justify-center h-[2.8rem] md:h-[3rem]  sm:px-6 lg:px-8">
         {/* Desktop Navigation */}
-        <div className="hidden md:flex md:items-center md:space-x-8 border bg-white shadow-sm  h-full px-16 rounded-full">
+        <div className="flex items-center space-x-4 md:space-x-8 border bg-white shadow-sm  h-full px-8 md:px-16 rounded-full">
           {navdata.map((item) => (
             <Link
               key={item.title}
               href={item.href}
               className={cn(
-                "text-sm font-medium transition-colors hover:text-primary",
+                "text-xs md:text-sm font-medium transition-colors hover:text-primary",
                 pathname === item.href
                   ? "text-primary"
                   : "text-muted-foreground"
@@ -31,7 +31,7 @@ const Navbar = () => {
         </div>
 
         {/* Mobile Menu Button */}
-        <div className="absolute right-4 md:hidden">
+        {/* <div className="absolute right-4 md:hidden">
           <button
             type="button"
             className="inline-flex items-center justify-center rounded-md p-2 text-muted-foreground hover:bg-accent hover:text-accent-foreground"
@@ -44,11 +44,11 @@ const Navbar = () => {
               <Menu className="h-6 w-6" />
             )}
           </button>
-        </div>
+        </div> */}
       </div>
 
       {/* Mobile Menu */}
-      <div className={cn("md:hidden", mobileMenuOpen ? "block" : "hidden")}>
+      {/* <div className={cn("md:hidden", mobileMenuOpen ? "block" : "hidden")}>
         <div className="space-y-1 px-4 pb-3 pt-2">
           {navdata.map((item) => (
             <Link
@@ -66,7 +66,7 @@ const Navbar = () => {
             </Link>
           ))}
         </div>
-      </div>
+      </div> */}
     </nav>
   );
 };
