c4045711651dadf4b6ff388fde4932b368f90b58
AyushKazi <ayushkazishrestha@gmail.com>
2025-03-07 12:32:39 +0545

    style: update AboutMe component layout and spacing for improved readability

--- src/components/about-me.tsx
+++ src/components/about-me.tsx
@@ -8,24 +8,24 @@ import { HiDownload } from "react-icons/hi";
 const AboutMe = () => {
   return (
     <div
-      className="flex flex-col-reverse md:flex-row w-full    scroll-mt-[10rem] gap-6 md:gap-0 pb-10 lg:pb-14 mt-[1rem]   md:mt-[6rem]"
+      className="flex flex-col-reverse md:flex-row w-full    scroll-mt-[10rem] gap-6 md:gap-0 pb-10 lg:pb-14 mt-[3rem]  md:mt-[6rem]"
       id="about-me"
     >
       {/* desc */}
-      <div className=" flex flex-col text-center md:text-left md:w-2/3 gap-4 md:gap-6">
+      <div className=" flex flex-col text-center md:text-left md:w-2/3 gap-4 md:gap-6  ">
         <h1 className="text-lg md:text-xl lg:text-3xl font-semibold">
           Hi there! 👋🏻
         </h1>
-        <h1 className="text-2xl md:text-3xl xl:text-5xl font-semibold">
+        <h1 className="text-2xl md:text-3xl -mt-1 xl:text-5xl font-semibold">
           I'm Ayush Kazi Shrestha 🙇🏻‍♂️
         </h1>
-        <p className="max-w-md lg:max-w-lg xl:max-w-2xl text-sm lg:text-base">
+        <p className="max-w-md mx-auto md:mx-0  lg:max-w-lg xl:max-w-2xl text-sm lg:text-base ">
           I am a software developer with a passion for creating web
           applications. I have experience with JavaScript, React, Node.js, and
           MongoDB. I am currently learning TypeScript and GraphQL. I am excited
           to learn new technologies and improve my skills as a developer.
         </p>
-        <div>
+        <div className="mx-auto md:mx-0">
           <a
             className="group text-white hover:scale-105 transition-all hover:bg-black/90 gap-2 w-fit bg-black/80 flex justify-center items-center  px-6 py-2 text-sm rounded-sm hover:cursor-pointer"
             download={true}
